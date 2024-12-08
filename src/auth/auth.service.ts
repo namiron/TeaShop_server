5c82b553b284ec473458c5a517eb63f4d75bf876
namiron <sasha_zpua@mail.ru>
2024-12-08 15:56:50 +0200

    fix_data_log

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,125 +1,124 @@
 import {
-	BadRequestException,
-	Injectable,
-	NotFoundException,
-	UnauthorizedException
-} from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
-import { JwtService } from '@nestjs/jwt'
-import { Response } from 'express'
-import { PrismaService } from 'src/prisma.service'
-import { UserService } from 'src/user/user.service'
-import { AuthDto } from './dto/auth.dto'
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { JwtService } from "@nestjs/jwt";
+import { Response } from "express";
+import { PrismaService } from "src/prisma.service";
+import { UserService } from "src/user/user.service";
+import { AuthDto } from "./dto/auth.dto";
 
 @Injectable()
 export class AuthService {
-	EXPIRE_DAY_REFRESH_TOKEN = 1
-	REFRESH_TOKEN_NAME = 'refreshToken'
+  EXPIRE_DAY_REFRESH_TOKEN = 1;
+  REFRESH_TOKEN_NAME = "refreshToken";
 
-	constructor(
-		private jwt: JwtService,
-		private userService: UserService,
-		private prisma: PrismaService,
-		private configService: ConfigService
-	) {}
+  constructor(
+    private jwt: JwtService,
+    private userService: UserService,
+    private prisma: PrismaService,
+    private configService: ConfigService,
+  ) {}
 
-	async login(dto: AuthDto) {
-		const user = await this.validateUser(dto)
+  async login(dto: AuthDto) {
+    const user = await this.validateUser(dto);
 
-		const tokens = this.issueTokens(user.id)
+    const tokens = this.issueTokens(user.id);
 
-		return { user, ...tokens }
-	}
+    return { user, ...tokens };
+  }
 
-	async register(dto: AuthDto) {
-		const oldUser = await this.userService.getByEmail(dto.email)
+  async register(dto: AuthDto) {
+    const oldUser = await this.userService.getByEmail(dto.email);
 
-		if (oldUser)
-			throw new BadRequestException('Пользователь уже существует')
+    if (oldUser) throw new BadRequestException("User already exists");
 
-		const user = await this.userService.create(dto)
+    const user = await this.userService.create(dto);
 
-		const tokens = this.issueTokens(user.id)
+    const tokens = this.issueTokens(user.id);
 
-		return { user, ...tokens }
-	}
+    return { user, ...tokens };
+  }
 
-	async getNewTokens(refreshToken: string) {
-		const result = await this.jwt.verifyAsync(refreshToken)
-		if (!result) throw new UnauthorizedException('Невалидный refresh токен')
+  async getNewTokens(refreshToken: string) {
+    const result = await this.jwt.verifyAsync(refreshToken);
+    if (!result) throw new UnauthorizedException("Invalid refresh token");
 
-		const user = await this.userService.getById(result.id)
+    const user = await this.userService.getById(result.id);
 
-		const tokens = this.issueTokens(user.id)
+    const tokens = this.issueTokens(user.id);
 
-		return { user, ...tokens }
-	}
+    return { user, ...tokens };
+  }
 
-	issueTokens(userId: string) {
-		const data = { id: userId }
+  issueTokens(userId: string) {
+    const data = { id: userId };
 
-		const accessToken = this.jwt.sign(data, {
-			expiresIn: '1h'
-		})
+    const accessToken = this.jwt.sign(data, {
+      expiresIn: "1h",
+    });
 
-		const refreshToken = this.jwt.sign(data, {
-			expiresIn: '7d'
-		})
+    const refreshToken = this.jwt.sign(data, {
+      expiresIn: "7d",
+    });
 
-		return { accessToken, refreshToken }
-	}
+    return { accessToken, refreshToken };
+  }
 
-	private async validateUser(dto: AuthDto) {
-		const user = await this.userService.getByEmail(dto.email)
+  private async validateUser(dto: AuthDto) {
+    const user = await this.userService.getByEmail(dto.email);
 
-		if (!user) throw new NotFoundException('Пользователь не найден')
+    if (!user) throw new NotFoundException("User not found");
 
-		return user
-	}
+    return user;
+  }
 
-	async validateOAuthLogin(req: any) {
-		let user = await this.userService.getByEmail(req.user.email)
+  async validateOAuthLogin(req: any) {
+    let user = await this.userService.getByEmail(req.user.email);
 
-		if (!user) {
-			user = await this.prisma.user.create({
-				data: {
-					email: req.user.email,
-					name: req.user.name,
-					picture: req.user.picture
-				},
-				include: {
-					stores: true,
-					favorites: true,
-					orders: true
-				}
-			})
-		}
+    if (!user) {
+      user = await this.prisma.user.create({
+        data: {
+          email: req.user.email,
+          name: req.user.name,
+          picture: req.user.picture,
+        },
+        include: {
+          stores: true,
+          favorites: true,
+          orders: true,
+        },
+      });
+    }
 
-		const tokens = this.issueTokens(user.id)
+    const tokens = this.issueTokens(user.id);
 
-		return { user, ...tokens }
-	}
+    return { user, ...tokens };
+  }
 
-	addRefreshTokenToResponse(res: Response, refreshToken: string) {
-		const expiresIn = new Date()
-		expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN)
+  addRefreshTokenToResponse(res: Response, refreshToken: string) {
+    const expiresIn = new Date();
+    expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN);
 
-		res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
-			httpOnly: true,
-			domain: this.configService.get('SERVER_DOMAIN'),
-			expires: expiresIn,
-			secure: true,
-			sameSite: 'none'
-		})
-	}
+    res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
+      httpOnly: true,
+      domain: this.configService.get("SERVER_DOMAIN"),
+      expires: expiresIn,
+      secure: true,
+      sameSite: "none",
+    });
+  }
 
-	removeRefreshTokenFromResponse(res: Response) {
-		res.cookie(this.REFRESH_TOKEN_NAME, '', {
-			httpOnly: true,
-			domain: this.configService.get('SERVER_DOMAIN'),
-			expires: new Date(0),
-			secure: true,
-			sameSite: 'none'
-		})
-	}
+  removeRefreshTokenFromResponse(res: Response) {
+    res.cookie(this.REFRESH_TOKEN_NAME, "", {
+      httpOnly: true,
+      domain: this.configService.get("SERVER_DOMAIN"),
+      expires: new Date(0),
+      secure: true,
+      sameSite: "none",
+    });
+  }
 }
