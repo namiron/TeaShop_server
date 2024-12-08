5d3cef56cd1662828233d0f12cf2a7a6701e095b
namiron <sasha_zpua@mail.ru>
2024-12-08 13:52:44 +0200

    port

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
-import * as cookieParser from 'cookie-parser'
+import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule);
 
-	app.use(cookieParser())
-	app.enableCors({
-		origin: [process.env.CLIENT_URL],
-		credentials: true,
-		exposedHeaders: 'set-cookie'
-	})
+  app.use(cookieParser());
+  app.enableCors({
+    origin: [process.env.CLIENT_URL],
+    credentials: true,
+    exposedHeaders: "set-cookie",
+  });
 
-	await app.listen(5000)
+  await app.listen(process.env.PORT || 5000);
 }
-bootstrap()
+bootstrap();
